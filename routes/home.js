b78aa8882e157a459a2007578a3ce940ceac6377
nj10-15 <nj29@illinois.edu>
2025-11-05 17:28:02 -0600

    updated routes

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,3 @@
-module.exports = function (router) {
-
-    var homeRoute = router.route('/');
-
-    homeRoute.get(function (req, res) {
-        var connectionString = process.env.TOKEN;
-        res.json({ message: 'My connection string is ' + connectionString });
-    });
-
-    return router;
-}
+module.exports = function home(req, res) {
+  res.status(200).json({ message: 'OK', data: { service: 'Llama.io API' } });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
-/*
- * Connect all of your endpoints together here.
- */
-module.exports = function (app, router) {
-    app.use('/api', require('./home.js')(router));
+const express = require('express');
+const notFound = require('../middleware/notFound');
+const errorHandler = require('../middleware/errorHandler');
+
+const userRoutes = require('./userRoutes');
+const taskRoutes = require('./taskRoutes');
+const home = require('./home');
+
+module.exports = function (app /*, router */) {
+  // home
+  app.get('/', home);
+
+  // api routes
+  app.use('/api/users', userRoutes);
+  app.use('/api/tasks', taskRoutes);
+
+  // errors
+  app.use(notFound);
+  app.use(errorHandler);
+
+  return app;
 };
