807b268d2e7ad2e38ded2cfc722ba92a922a5891
nj10-15 <nj29@illinois.edu>
2025-11-04 23:36:51 -0600

    updated few files

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
-// Get the packages we need
-var express = require('express'),
-    router = express.Router(),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser');
+import 'dotenv/config';
+import { createServer } from 'http';
+import app from './src/app.js';
+import connectDB from './src/db.js';
 
-// Read .env file
-require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
-// Create our Express application
-var app = express();
-
-// Use environment defined port or 3000
-var port = process.env.PORT || 3000;
-
-// Connect to a MongoDB --> Uncomment this once you have a connection string!!
-mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true });
-
-// Allow CORS so that backend and frontend could be put on different servers
-var allowCrossDomain = function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
-};
-app.use(allowCrossDomain);
-
-// Use the body-parser package in our application
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
-// Use routes as a module (see index.js)
-require('./routes')(app, router);
-
-// Start the server
-app.listen(port);
-console.log('Server running on port ' + port);
+(async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    const server = createServer(app);
+    server.listen(PORT, () => {
+      console.log(`API running on http://localhost:${PORT}/api`);
+    });
+  } catch (err) {
+    console.error('Failed to start:', err);
+    process.exit(1);
+  }
+})();
